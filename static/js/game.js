5b1a78162af1d74b1f9112ed1534219a3ab52849
Jerry Ye <yejerry333@gmail.com>
2017-06-07 16:59:49 -0400

    Added button labels

--- static/js/game.js
+++ static/js/game.js
@@ -14,6 +14,9 @@ var income = 1;
 var cash = 0;
 var old_time = 0;
 var time = 0;
+var home_rent = 0;
+var car_pay = 0;
+var pet_pay = 0;
 
 function preload() {
   game.load.spritesheet('button', 'static/images/buttons.png', 160, 40);
@@ -23,15 +26,17 @@ function create() {
 
   game.stage.backgroundColor = '#fffff0';
 
-  credit_score_text = game.add.text(10, 20, "Credit Score: " + credit_score, { font: "12px Arial", fill: "000000"});
-  income_text = game.add.text(10, 570, "Income: " + income + "/hr", { font: "12px Arial", fill: "000000"});
-  cash_text = game.add.text(game.world.centerX-50, 570, "Cash: " + cash, { font: "12px Arial", fill: "000000"});
-  time_text = game.add.text(680, 570, "Time: " + time + " hours", { font: "12px Arial", fill: "000000"});
-
   rent_button = game.add.button(10, 60, 'button', test, this, 0, 0, 2);
   pet_button = game.add.button(10, 90, 'button', test, this, 0, 0, 2);
   car_button = game.add.button(10, 120, 'button', test, this, 0, 0, 2);
 
+  credit_score_text = game.add.text(10, 20, "Credit Score: " + credit_score, { font: "12px Arial", fill: "000000"});
+  income_text = game.add.text(10, 570, "Income: " + income + "/hr", { font: "12px Arial", fill: "000000"});
+  cash_text = game.add.text(game.world.centerX-50, 570, "Cash: " + cash, { font: "12px Arial", fill: "000000"});
+  time_text = game.add.text(680, 570, "Time: " + time + " hours", { font: "12px Arial", fill: "000000"});
+  home_rent_text = game.add.text(20, 67, "Pay rent: " + home_rent,  { font: "12px Arial", fill: "000000"});
+  car_pay_text = game.add.text(20, 97, "Pay car fee: " + car_pay,  { font: "12px Arial", fill: "000000"});
+  pet_pay_text = game.add.text(20, 127, "Pay pet fee: " + pet_pay, { font: "12px Arial", fill: "000000"})
 }
 
 function test() {
